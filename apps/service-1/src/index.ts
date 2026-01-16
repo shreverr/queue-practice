9e00388064a5ce519bba8752a33264db090ce0e7
Shreshth Verma <verma2007s@gmail.com>
2026-01-16 19:36:07 +0530

    feat: added video upload endpoint

--- apps/service-1/src/index.ts
+++ apps/service-1/src/index.ts
@@ -1,9 +1,37 @@
 import { Hono } from 'hono'
+import { Queue } from 'bullmq';
 
 const app = new Hono()
 
+const videoTranscodingQueue = new Queue('video-transcoding', {
+  connection: {
+    host: "localhost",
+    port: 6379
+  }
+});
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.post("/video-upload", async (c) => {
+  const { videoUrl, username } = await c.req.json()
+
+  if (!videoUrl || !username) {
+    return c.json({
+      success: false,
+      message: "videoUrl and username are required"
+    }, 400)
+  }
+
+  videoTranscodingQueue.add("transcode", {
+    videoUrl, username
+  })
+
+  return c.json({
+      success: true,
+      message: "job accepted"
+    }, 200)
 })
 
 export default app
